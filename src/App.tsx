214af1faf04055df83afec241aca321550f16aab
Pawel-W88 <rws.pawlak@gmail.com>
2025-01-21 12:43:09 +0100

    feat: add basic ui for task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+interface Task {
+  id: string,
+  text: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value)
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== ''){
+      setTasks([...tasks, {id: crypto.randomUUID(), text: newTask}]);
+      setNewTask('');
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <h1>ToDoList</h1>
+      <input type="text" value={newTask} onChange={handleInputChange} />
+      <button onClick={handleAddTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.text}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
