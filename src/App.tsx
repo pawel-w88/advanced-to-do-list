0165fb3c3114a7acf9e253ebad57e52884cd9759
Pawel-W88 <rws.pawlak@gmail.com>
2025-01-22 14:22:53 +0100

    feat: Add task categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,60 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 interface Task {
-  id: string,
-  text: string
+  id: string;
+  text: string;
+  category: string;
 }
 
 function App() {
-
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTask(event.target.value)
+    setNewTask(event.target.value);
   };
 
   const handleAddTask = () => {
-    if (newTask.trim() !== ''){
-      setTasks([...tasks, {id: crypto.randomUUID(), text: newTask}]);
-      setNewTask('');
+    if (newTask.trim() !== "") {
+      setTasks([
+        ...tasks,
+        { id: crypto.randomUUID(), text: newTask, category: "other" },
+      ]);
+      setNewTask("");
     }
-  }
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>ToDoList</h1>
       <input type="text" value={newTask} onChange={handleInputChange} />
       <button onClick={handleAddTask}>Add Task</button>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>{task.text}</li>
+          <li key={task.id}>
+            {task.text}
+            <select
+              value={task.category}
+              onChange={(e) => {
+                const updatedTasks = tasks.map((t) => {
+                  if (t.id === task.id) {
+                    return { ...t, category: e.target.value };
+                  }
+                  return t;
+                });
+                setTasks(updatedTasks);
+              }}
+            >
+              <option value="work">Work</option>
+              <option value="house">House</option>
+              <option value="other">Other</option>
+            </select>
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
